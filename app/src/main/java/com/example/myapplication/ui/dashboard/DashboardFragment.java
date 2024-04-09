c1698ad40d7590d92980f3d31d614f56aeed7598
MuffinHD <125144775+MuffinHD@users.noreply.github.com>
2024-04-09 16:15:46 +0200

    ...

--- app/src/main/java/com/example/myapplication/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/myapplication/ui/dashboard/DashboardFragment.java
@@ -26,7 +26,7 @@ public class DashboardFragment extends Fragment {
 
         final TextView textView = binding.textDashboard;
         dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        return root; //überprüfen
     }
 
     @Override
